d0755cd1af579a0153f2a578f1f62b93ad9d86cb
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-06-30 14:10:10 +0300

    fix: background size update

--- src/components/Hero/Hero.styled.jsx
+++ src/components/Hero/Hero.styled.jsx
@@ -44,7 +44,12 @@ export const BackgroundContainer = styled.section`
 
     @media screen and (min-width: 1170px) {
       background-image: url(${backgroundDesktop});
-      background-size: 1170px;
+      background-size: 1170px auto;
+      @media (min-device-pixel-ratio: 2),
+        (min-resolution: 192dpi),
+        (min-resolution: 2dppx) {
+        background-image: url(${backgroundDesktopRetina});
+      }
     }
   }
 
