79b3db3b81a76103f8cfd73dcae2504738673ffb
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-06-30 15:17:32 +0300

    fix: radio buttons updated

--- src/components/SignUp/Select/Select.styled.jsx
+++ src/components/SignUp/Select/Select.styled.jsx
@@ -12,7 +12,39 @@ export const SelectContainer = styled.div`
   }
 
   .form__select-input {
+    display: inline-flex;
+    align-items: center;
+    vertical-align: sub;
+    position: relative;
     margin-right: 12px;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 20px;
+    border: 1px solid #d0cfcf;
+    outline: none;
+    transition: border-color 0.3s ease;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 10px;
+      width: 10px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 50%;
+      background-color: transparent;
+      transition: background-color 0.3s ease;
+    }
+
+    &:checked::before {
+      background-color: #00bdd3;
+    }
+
+    &:checked {
+      border-color: #00bdd3;
+    }
   }
 `;
 
